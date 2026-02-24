670a8ddeab08a0422225ed808e7e1ce67b7b2184
Kari Puzzonia <126693423+kpuzzonia@users.noreply.github.com>
2026-02-24 12:46:04 -0500

    Finish functionality of contact form

--- formValidation.js
+++ formValidation.js
@@ -6,8 +6,38 @@ contactForm.addEventListener('submit', function(event) {
     let fullName = document.getElementById('fullName').value;
     let emailAddress = document.getElementById('emailAddress').value;
     let message = document.getElementById('message').value;
+    let errorMessage = document.getElementById('errorMessage');
+    let successMessage = document.getElementById('successMessage');
+
 
     if (fullName.trim() == "" || emailAddress.trim() == "" || message.trim() == "") {
+        errorMessage.style.display = "block";
+        errorMessage.textContent = "Please complete all fields."
 
+        return;
+    }
+    else {
+        let formData = new FormData(contactForm);
+        fetch('https://formspree.io/f/xvzbdzpg', {
+            method: 'POST',
+            body: formData,
+            headers: {
+            'Accept': 'application/json'
+        }
+    }).then(function(response) {
+        if (response.ok) {
+            // show success message and reset form here
+            errorMessage.style.display = "none";
+            successMessage.style.display = "block";
+            successMessage.textContent= "Thank you for your submission. You will be contacted within 2-3 business days."
+            contactForm.reset();
+        } else {
+            // show error message here
+            errorMessage.style.display = "block";
+            errorMessage.textContent = "Something went wrong, please try again."
+        }
+    })
+
+        
     }
 })
