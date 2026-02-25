57a459f3ea30e030c2eefd7dbd7d4509f3ab6024
Kari Puzzonia <126693423+kpuzzonia@users.noreply.github.com>
2026-02-25 11:54:02 -0500

    Finish styling, ready to launch

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,6 +1,13 @@
 let hamburgerButton = document.getElementById('hamburgerButton');
 let mobileNavLinks = document.getElementById('mobileNavLinks');
+let navLinks = document.querySelectorAll('#mobileNavLinks a');
 
 hamburgerButton.addEventListener('click', function(event) {
     mobileNavLinks.classList.toggle('open');
 })
+
+navLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        mobileNavLinks.classList.remove('open');
+    });
+});
